424c555c48fe80def4ce0c27599baa48cf4f960d
MateusFujita <mateusfugama@yahoo.com.br>
2024-09-05 14:25:49 -0300

    Finalização das atualizações para clientes não logados

--- loja/new_context.py
+++ loja/new_context.py
@@ -1,12 +1,16 @@
-from .models import Pedido, ItemPedido
+from .models import Pedido, ItemPedido, Cliente
 
 def carrinho(request):
     quantidadeProdutosCarrinho = 0
     if request.user.is_authenticated:
         cliente = request.user.cliente
     else:
-        return {"quantidadeProdutosCarrinho": quantidadeProdutosCarrinho}
-    pedido, criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+        if request.COOKIES.get('idSessao'):
+            idSessao = request.COOKIES.get('idSessao')
+            cliente, criado = Cliente.objects.get_or_create(idSessao = idSessao)
+        else:
+            return {"quantidadeProdutosCarrinho": quantidadeProdutosCarrinho}  
+    pedido,criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
     itemPedido = ItemPedido.objects.filter(pedido=pedido)
     for item in itemPedido:
         quantidadeProdutosCarrinho += item.quantidade
